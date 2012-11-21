be6ebbd09716c7f04d7982670147267aa295df64
aumgn <aumgn@free.fr>
2012-11-22 00:31:30 +0100

    Clean up yaml pages controller

--- app/controllers/yaml.rb
+++ app/controllers/yaml.rb
@@ -1,28 +1,32 @@
 class EthilVan::App < Sinatra::Base
 
-   YamlPages = []
-   EthilVan.load_datas("pages").each do |data|
-       data.each do |id, hash|
-         YamlPages << Views::YamlPage.new(id, hash)
+   def self.define_simple_page(page)
+      get page.url do
+         view page
+         mustache page.template
       end
    end
 
-   YamlPages.each do |yaml_page|
-      if yaml_page.tabs?
-         get yaml_page.url do
-            redirect yaml_page.main_tab_url
-         end
+   def self.define_page_with_tabs(page)
+      get page.url do
+         redirect page.main_tab_url
+      end
 
-         for tab in yaml_page.tabs do
-            get tab.url do
-               view yaml_page
-               mustache yaml_page.template
-            end
+      for tab in page.tabs do
+         get tab.url do
+            view page
+            mustache page.template
          end
-      else
-         get yaml_page.url do
-            view yaml_page
-            mustache yaml_page.template
+      end
+   end
+
+   EthilVan.load_datas("pages").each do |data|
+       data.each do |id, hash|
+         yaml_page = Views::YamlPage.new(id, hash)
+         if yaml_page.tabs?
+            define_page_with_tabs yaml_page
+         else
+            define_simple_page yaml_page
          end
       end
    end
