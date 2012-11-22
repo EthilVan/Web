81cad205004a06a76c07aa4c206c4f708fa150fb
aumgn <aumgn@free.fr>
2012-11-22 20:16:50 +0100

    Add Database default config

--- config/database.rb
+++ config/database.rb
@@ -1,8 +1,17 @@
 require 'active_record'
 
 class ActiveRecord::Base
+
+   default_config = {
+      adapter:   'mysql2',
+      encoding:  'utf8',
+      reconnect: true,
+      pool:      5,
+      host:      'localhost',
+   }
+
    EthilVan::Config['database'].each do |env, dbconfig|
-      configurations[env.to_sym] = dbconfig
+      configurations[env.to_sym] = default_config.merge(dbconfig)
    end
 
    establish_connection configurations[EthilVan::ENV]
