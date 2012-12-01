655ea0f086e2dfe5f9dfc9293e9c7308f0b865a9
aumgn <aumgn@free.fr>
2012-12-02 00:23:58 +0100

    Fix watchr

--- tasks/assets/watchr.rb
+++ tasks/assets/watchr.rb
@@ -4,28 +4,37 @@ class EthilVan::Assets::Watchr < ::Watchr::Script
 
    def initialize(assets)
       super()
-
-      Style.included_files.each do |file|
-         watch file do
-            Style.each &:compile
-         end
-      end
-
-      assets.each do |asset|
-         watch asset.manifest do
-            watch_asset_files asset
-            asset.compile
-         end
-         watch_asset_files asset
-      end
-
+      @assets = assets
+      watch_all
       puts 'Watching'
    end
 
-   def watch_asset_files(asset)
-      asset.files.each do |file|
-         watch file do |md|
-            asset.compile
+   def watch_all
+      reset
+
+      Style.included_files.each do |file|
+         watch(file) { Style.each &:compile }
+      end
+
+      @assets.each do |asset|
+         watch asset.manifest do
+            watch_all
+            asset.each &:compile
+         end
+      end
+
+      assets_by_file.each do |file, assets|
+         watch(file) { assets.each &:compile }
+      end
+   end
+
+private
+
+   def assets_by_file
+      @assets.each_with_object({}) do |asset, hash|
+         asset.files.each do |file|
+            hash[file] ||= []
+            hash[file] << asset
          end
       end
    end
