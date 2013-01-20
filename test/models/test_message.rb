0c42ba73363a720ace0eaefac26f7fde012da788
aumgn <aumgn@free.fr>
2013-01-20 14:04:20 +0100

    Ajout d'un test pour le parsing markdown des messages

--- test/models/test_message.rb
+++ test/models/test_message.rb
@@ -12,3 +12,32 @@ class MessageTest < MiniTest::Spec
       @message.wont_be_valid_with contents: ''
    end
 end
+
+class MessageMarkdownTest < MiniTest::Spec
+
+   def setup
+      DatabaseCleaner.start
+      @message = Message.new
+      @message.account = Account.first
+   end
+
+   def teardown
+      DatabaseCleaner.clean
+   end
+
+   def test_contents_is_automatically_parsed_when_saved
+      @message.contents = <<-MSG
+**Awesome Message Here !**
+      MSG
+      @message.parsed_contents.wont_be :present?
+      @message.save
+      @message.parsed_contents.must_be :present?
+      @message.parsed_contents.must_match /<strong>Awesome Message Here !<\/strong>/ #/
+
+      @message.contents = <<-MSG
+**Edited Message Here !**
+      MSG
+      @message.save
+      @message.parsed_contents.must_match /<strong>Edited Message Here !<\/strong>/
+   end
+end
