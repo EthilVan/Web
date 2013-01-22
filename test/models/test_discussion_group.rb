63fa6ee592bdd7882e4e7838800f90a6e6b50523
aumgn <aumgn@free.fr>
2013-01-22 12:19:54 +0100

    Validations groupe de discussion

---
+++ test/models/test_discussion_group.rb
@@ -0,0 +1,22 @@
+require_relative 'helpers'
+
+class DiscussionGroupTest < MiniTest::Spec
+
+   def setup
+      @group = GeneralDiscussionGroup.new
+   end
+
+   def test_name_validation
+      @group.must_be_valid_with name: 'Nom'
+      @group.wont_be_valid_with name: nil
+      @group.wont_be_valid_with name: ''
+   end
+
+   def test_url_validation
+      @group.must_be_valid_with url: 'url_du_groupe'
+      @group.wont_be_valid_with url: nil
+      @group.wont_be_valid_with url: ''
+      @group.wont_be_valid_with url: 'a/a'
+      @group.wont_be_valid_with url: 'a%'
+   end
+end
