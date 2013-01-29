245eaf32c9f3f72c829fafafc0141c08de1ea359
aumgn <aumgn@free.fr>
2013-01-29 19:39:42 +0100

    Ajout de la page /markdown/membres.json

--- app/controllers/public/root.rb
+++ app/controllers/public/root.rb
@@ -1,4 +1,8 @@
+require 'sinatra/json'
+
 class EthilVan::App < Sinatra::Base
+
+   helpers Sinatra::JSON
 
    get '/' do
       redirect '/presentation/generale'
@@ -6,5 +10,9 @@ class EthilVan::App < Sinatra::Base
 
    post '/markdown' do
       markdown params['content']
+   end
+
+   get '/markdown/membres.json' do
+      json Account.where("role_id != 'ancien'").pluck(:name)
    end
 end
