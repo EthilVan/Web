18b8d3b57727869d17e4f7c1431f54b8ae50f4cb
aumgn <aumgn@free.fr>
2013-02-03 10:25:13 +0100

    [Form Postulation] Réorganisation des templates

--- app/controllers/public/postulation.rb
+++ app/controllers/public/postulation.rb
@@ -1,20 +1,20 @@
 class EthilVan::App
 
-   get '/postulation/formulaire' do
+   get '/postulation' do
       postulation = Postulation.new
       postulation.screens = (1..3).map { PostulationScreen.new }
       view Views::Public::Postulation::Formulaire.new postulation
-      mustache 'public/postulation/formulaire'
+      mustache 'public/postulation/tabs'
    end
 
-   post '/postulation/formulaire' do
+   post '/postulation' do
       postulation = Postulation.new params[:postulation]
       if postulation.save
          view Views::Public::Postulation::Validation.new
          mustache 'public/postulation/validation'
       else
          view Views::Public::Postulation::Formulaire.new postulation
-         mustache 'public/postulation/formulaire'
+         mustache 'public/postulation/tabs'
       end
    end
 end
