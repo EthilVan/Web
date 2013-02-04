97ef5e4808b82c183f9c5314c9e3c7126fb12664
aumgn <aumgn@free.fr>
2013-02-04 18:42:02 +0100

    [Skins] MaJ beaucoup plus rapide avec les threads

--- lib/ethilvan/skins/updater.rb
+++ lib/ethilvan/skins/updater.rb
@@ -7,8 +7,10 @@ module EthilVan::Skins
       MINECRAFT_BASE_URL = 'http://s3.amazonaws.com/MinecraftSkins/'
 
       def self.update_all
-         new('char').update
-         accounts_from_db.each { |account| new(account).update }
+         puts 'Updating all skins ...'
+         thread = [new('char').update]
+         thread += accounts_from_db.map { |account| new(account).update }
+         sleep(10) while thread.any?(&:alive?)
       end
 
       def self.accounts_from_db
@@ -33,52 +35,35 @@ module EthilVan::Skins
       end
 
       def update
-         puts_line
-         puts "Updating '#{@name}' skin"
-         do_update
-         puts_line
+         Thread.new do
+            status = do_update
+            puts "#@name ... #{status} !"
+         end
       end
 
    private
 
       def do_update
-         puts 'Downloading ...'
          response = Net::HTTP.get_response skin_uri_for @name
-         if response.code != '200'
-            puts 'Raw skin not found'
-            return
-         end
+         return 'Not Found' if response.code != '200'
 
          last_modified = response['Last-Modified']
          last_modified = Time.parse last_modified
-         unless @raw.need_update? last_modified
-            puts 'Skin up-to-date'
-            return
-         end
+         return 'Up-to-date' unless @raw.need_update? last_modified
 
-         puts 'Replacing raw skin'
          @raw.replace(response.body)
 
          PREGENERATED.each do |klass, scales|
             scales.each { |scale| update_generated(klass, scale) }
          end
+         return 'Updated'
       end
 
       def update_generated(klass, scale)
-         puts "Generating #{klass.name.downcase} at scale #{scale}"
          generated = klass.new(@name, scale, @raw)
          if generated.need_update? @raw.mtime
             generated.generate
          end
-      end
-
-      def puts(*args)
-         first = args.shift
-         super('#  ' + first, *args)
-      end
-
-      def puts_line
-         Kernel.puts('# ' + '=' * 60)
       end
 
       def skin_uri_for(username)
