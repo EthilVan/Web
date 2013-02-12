57897bd2085b072001ac377fb8251f649ec5468c
aumgn <aumgn@free.fr>
2013-02-12 10:59:28 +0100

    [Profil] Ajout de la méthode `éditable?`

--- app/views/member/profil/_general.rb
+++ app/views/member/profil/_general.rb
@@ -12,6 +12,10 @@ module EthilVan::App::Views
             @minecraft_stats = account.minecraft_stats
          end
 
+         def editable?
+            modo? or @app.current_account.id == @account.id
+         end
+
          def cadre
             Cadre.new(@account, @profil, @minecraft_stats)
          end
