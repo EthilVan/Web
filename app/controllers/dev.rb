912a797b593c80ed011bbad782bf852db082d713
aumgn <aumgn@free.fr>
2013-02-12 13:31:51 +0100

    [Development] Ajout des routes /dev/login/<pseudo> et /dev/role/<role>

---
+++ app/controllers/dev.rb
@@ -0,0 +1,19 @@
+EthilVan.development? do
+
+   class EthilVan::App < Sinatra::Base
+
+      get '/dev/login/:account' do |account_name|
+         account = Account.where(name: account_name).first
+         halt "Pas de membre avec le pseudo #{account_name}" if account.nil?
+         token = account.generate_auth_token
+         login(account.name, token, false)
+         redirect '/'
+      end
+
+      logged_only %r{^/dev/role/}
+      get '/dev/role/:new_role' do |new_role|
+         current_account.update_attribute :role_id, new_role
+         redirect '/'
+      end
+   end
+end
