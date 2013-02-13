c6ea47a13d5357d3202c881d996b7a089ae93728
aumgn <aumgn@free.fr>
2013-02-13 12:15:40 +0100

    [Gestion Postulation] Fix

--- app/models/postulation_vote_observer.rb
+++ app/models/postulation_vote_observer.rb
@@ -2,21 +2,27 @@ class PostulationVoteObserver < ActiveRecord::Observer
 
    def after_create(vote)
       postulation = vote.postulation
+      visitor = Visitor.where(name: postulation.minecraft_name)
       if vote.agreement_needed?
          agreements = postulation.agreements_needed.size
          if agreements >= PostulationVote.total_needed
+            visitor.destroy_all
             postulation.update_attribute :status, 2
             Account.create_from_postulation(postulation)
             EthilVan.logger.info 'Sending final approbation mail'
+         elsif agreements == 1
+            visitor.first_or_create
+            EthilVan.logger.info 'Sending first approbation mail'
          else
             EthilVan.logger.info 'Sending approbation mail'
          end
       elsif vote.refusal?
          postulation.update_attribute :status, 1
          EthilVan.logger.info 'Sending refusal mail'
+      else
+         EthilVan.logger.info 'Sending dummy approbation mail'
       end
    end
 end
 
-# ActiveRecord::Base.add_observer PostulationVoteObserver.instance
 ActiveRecord::Base.observers = PostulationVoteObserver
--- content/scripts/app/postulation-vote.js
+++ content/scripts/app/postulation-vote.js
@@ -1,7 +1,14 @@
 $(document).on('click.ethilvan.postulation-vote',
       '.postulation-vote-button', function(event) {
    event.preventDefault();
+
+   if ($(this).is('.disabled')) {
+      return;
+   }
+
    $('#form-postulation-vote').slideDown(800);
    $('#postulation_vote_agreement').attr('checked',
-      $(this).attr('href') == '#agreement');
+         $(this).attr('href') == '#agreement');
+   $('.postulation-vote-button').removeClass('disabled');
+   $(this).addClass('disabled');
 });
