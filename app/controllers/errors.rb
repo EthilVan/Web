008db731f2200bb3f44e0b60f9661a0a2bf8c58a
aumgn <aumgn@free.fr>
2013-02-20 17:10:49 +0100

    [Erreur] Ajout de la page d'erreur d'autorisation

--- app/controllers/errors.rb
+++ app/controllers/errors.rb
@@ -1,5 +1,10 @@
 class EthilVan::App < Sinatra::Base
 
+   error 401 do
+      view Views::Error::NotAuthorized.new
+      mustache 'error/not_authorized'
+   end
+
    not_found do
       view Views::Error::NotFound.new
       mustache 'error/not_found'
---
+++ app/views/error/not_authorized.rb
@@ -0,0 +1,8 @@
+module EthilVan::App::Views
+
+   module Error
+
+      class NotAuthorized < Page
+      end
+   end
+end
