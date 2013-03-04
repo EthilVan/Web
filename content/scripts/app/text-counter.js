def7f746cb51ca55b00686edbb6c12f5a83b10c5
aumgn <aumgn@free.fr>
2013-03-04 10:20:17 +0100

    [Text Counter] Réécriture

--- content/scripts/app/text-counter.js
+++ content/scripts/app/text-counter.js
@@ -1,18 +1,83 @@
-$(function() {
+var TextCounter = function($element) {
+   this.init($element);
+};
 
-   $('[data-text-counter]').each(function() {
-      var $this = $(this);
-      var data = $this.data();
+TextCounter.prototype = {
 
-      var min = data.textCounterMin || data.minlength || null;
-      var max = data.textCounterMin || data.maxlength || null;
+   init: function($element) {
+      this.$element = $element;
+      this.$counterWrapper = $element.parent().find('[data-text-counter-wrapper]');
+      this.$counter = this.$counterWrapper.find('[data-text-counter-target]');
 
-      $this.miniCount({
-         min: min,
-         max: max,
-         className: 'text-counter',
-         validClassName: 'text-counter-valid',
-         invalidClass: 'text-counter-invalid',
-      });
-   });
+      this.state = null;
+      var data = this.$element.data();
+      this.min = data.textCounterMin || data.minlength || null;
+      this.max = data.textCounterMin || data.maxlength || null;
+   },
+
+   setState: function(value, klass) {
+      if (this.state == value) {
+         return;
+      }
+
+      this.state = value;
+      this.setClass(klass);
+   },
+
+   setClass: function(klass) {
+      this.$element.removeClass(
+            'text-counter-idle text-counter-invalid text-counter-valid');
+      this.$counterWrapper.removeClass(
+            'text-counter-idle text-counter-invalid text-counter-valid');
+      this.$element.addClass(klass);
+      this.$counterWrapper.addClass(klass);
+   },
+
+   setIdle: function() {
+      this.setState(null, 'text-counter-idle');
+      this.setCount('-');
+   },
+
+   setInvalid: function() {
+      this.setState(false, 'text-counter-invalid');
+   },
+
+   setValid: function() {
+      this.setState(true, 'text-counter-valid');
+   },
+
+   setCount: function(count) {
+      this.$counter.text(count);
+   },
+
+   update: function() {
+      var count  = this.$element.val().length;
+
+      if ((this.min && count < this.min) || (this.max && count > this.max)) {
+         this.setInvalid();
+      } else {
+         this.setValid();
+      }
+
+      this.setCount(count);
+   },
+}
+
+$.fn.textCounter = function(method) {
+   var $element = $(this);
+
+   var object = $element.data('text-counter-object');
+   if (!object) {
+      object = new TextCounter($element);
+      $element.data('text-counter-object', object);
+   }
+
+   if (method) {
+      object[method]();
+   }
+};
+
+$(document).on('focus.text-counter keyup.text-counter change.text-counter',
+      '[data-text-counter]', function() {
+   $(this).textCounter('update');
 });
